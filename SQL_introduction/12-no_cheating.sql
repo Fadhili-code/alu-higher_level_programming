da837d82a90c5012240e10e27d02598fc667a793
Fadhili-code <f.lumumba@alustudent.com>
2025-03-17 06:18:41 -0700

    commit

--- SQL_introduction/12-no_cheating.sql
+++ SQL_introduction/12-no_cheating.sql
@@ -1,5 +1,5 @@
--- A script that lists all records with a score >= 10 in the table second_table of the database hbtn_0c_0 in your MySQL server.
--- Query to list all records with a score >= 10
-SELECT score, name FROM second_table
-WHERE score >= 10
-ORDER BY score DESC;
+-- A script that updates the score of Bob to 10 in the table second_table
+-- Query to update the score of BOB to 10
+UPDATE second_table
+SET score = 10
+WHERE name='Bob'; 
